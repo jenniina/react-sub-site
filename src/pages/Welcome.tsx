5673019406aa167111b37d55402a7ae14c8f0ab4
Jenniina <jenniina@jenniina.fi>
2023-10-09 11:08:36 +0300

    add link to Joke app to welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,6 +5,7 @@ import Hero from '../components/Hero/Hero'
 import { BiChat } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
 import { IoMdImages } from 'react-icons/io'
+import { GiAbstract019 } from 'react-icons/gi'
 
 export default function Home({
   heading,
@@ -43,6 +44,20 @@ export default function Home({
                 <Link to='/contact'>
                   <BiChat /> <span>Contact</span>
                 </Link>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className={`card`}>
+          <div>
+            <h2 className={`${styles.subheading}`}>Newest additions</h2>
+            <ul className={`${styles.list}`}>
+              <li>
+                <Link to='/portfolio/jokes'>
+                  <GiAbstract019 />
+                  <span>Jokes App</span>
+                </Link>
+                <p>The Comedian's Companion</p>
               </li>
             </ul>
           </div>
