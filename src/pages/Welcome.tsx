698191abdbfd40fa79aa42f8acebed03ac915d30
Jenniina <jenniina@jenniina.fi>
2023-12-15 15:15:12 +0200

    Add recent changes list

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,6 +5,9 @@ import Hero from '../components/Hero/Hero'
 import { BiChat } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
 import { IoMdImages } from 'react-icons/io'
+import { GiAbstract019 } from 'react-icons/gi'
+import { MdOutlineQuiz } from 'react-icons/md'
+import { RiDragMove2Fill } from 'react-icons/ri'
 
 export default function Home({
   heading,
@@ -45,6 +48,32 @@ export default function Home({
                 </Link>
               </li>
             </ul>
+            <div className={`${styles.newest}`}>
+              <h2 className={`${styles.subheading}`}>Newest / Edits</h2>
+              <ul className={`${styles.list}`}>
+                <li>
+                  <Link to='/portfolio/quiz'>
+                    <MdOutlineQuiz />
+                    <span>Quiz</span>
+                  </Link>
+                  <p>Test your knowledge</p>
+                </li>
+                <li>
+                  <Link to='/portfolio/jokes'>
+                    <GiAbstract019 />
+                    <span>Jokes</span>
+                  </Link>
+                  <p>The Comedian's Companion</p>
+                </li>
+                <li>
+                  <Link to='/portfolio/blob'>
+                    <RiDragMove2Fill />
+                    <span>Blobs</span>
+                  </Link>
+                  <p>Make blob art your thing</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </section>
       </div>
