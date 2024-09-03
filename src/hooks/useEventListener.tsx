245536d14d4868e87de9ecc1e7690b0d6c3fc8b3
Jenniina <jenniina@jenniina.fi>
2024-09-03 11:42:34 +0300

    Edit useEventListener  hook and fix eye rotation issue

--- src/hooks/useEventListener.tsx
+++ src/hooks/useEventListener.tsx
@@ -1,22 +1,21 @@
-import React, { useEffect, useRef } from "react"
-
+import React, { useEffect, useRef } from 'react'
 
 export default function useEventListener(
-    eventType: keyof WindowEventMap,
-    callback: Function,
-    element = window
+  eventType: keyof WindowEventMap,
+  callback: (e: Event) => void,
+  element = window
 ) {
-    const callbackRef = useRef(callback)
+  const callbackRef = useRef(callback)
 
-    useEffect(() => {
-        callbackRef.current = callback
-    }, [callback])
+  useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
 
-    useEffect(() => {
-        if (element == null) return
-        const handler = (e: Event) => callbackRef.current(e)
-        element.addEventListener(eventType, handler)
+  useEffect(() => {
+    if (element == null) return
+    const handler = (e: Event) => callbackRef.current(e)
+    element.addEventListener(eventType, handler)
 
-        return () => element.removeEventListener(eventType, handler)
-    }, [eventType, element])
+    return () => element.removeEventListener(eventType, handler)
+  }, [eventType, element])
 }
--- src/pages/testpage.tsx
+++ src/pages/testpage.tsx
@@ -33,8 +33,8 @@ export default function Home({
   const { array, set, push, remove, filter, update, clear } = useArray([1, 2, 3, 4, 5, 6])
 
   const [key, setKey] = useState('')
-  useEventListener('keydown', (e: KeyboardEvent) => {
-    setKey(e.key)
+  useEventListener('keydown', (e: Event) => {
+    setKey((e as KeyboardEvent).key)
   })
 
   const headerTwoRef = useRef() as RefObject<HTMLElement>
