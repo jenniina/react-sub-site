73188be43e3eb0fe150a82856616694e12014506
Jenniina <jenniina@jenniina.fi>
2024-12-08 21:08:33 +0200

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    //react(),
+    react(),
     {
       ...copy({
         targets: [{ src: 'routes.json', dest: 'dist' }],
